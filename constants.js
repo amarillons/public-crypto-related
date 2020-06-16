163be5f9525630691cb5ee7ca4f51fec1cb666df
a.f <akihiko.k.fujii@gmail.com>
2020-06-16 17:45:46 +0900

    Create new branch

--- constants.js
+++ constants.js
@@ -3,6 +3,7 @@ constants = {
     tokenAddress: process.env.TOKEN_ADDRESS,
     eoaAddressForTesting: process.env.EOA_ADDRESS_TESTING,
     eoaAddressForTesting2: process.env.EOA_ADDRESS_TESTING2,
+    eoaAddressForTesting2PrivateKey: process.env.EOA_ADDRESS_TESTING2_PRIVATE_KEY,
 }
 
 module.exports = constants;
