4e71fe651d733c7cb7424379ad587f490514972b
a.f <akihiko.k.fujii@gmail.com>
2020-06-16 10:06:59 +0900

    Update functions

--- functions.js
+++ functions.js
@@ -6,6 +6,9 @@ const functions = {
     func2: (x) => {
         return 123;
     },
+    func3: (x) => {
+        return x * x;
+    }
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -4,5 +4,9 @@ test('testing func2', () => {
     // expect(functions.add(2,2)).toBe(4);
     expect(functions.func2(4)).toBe(123);
     expect(functions.func2(4)).not.toBe(1234);
-    // expect(functions.getBalance('0x56416D060Bf9b0cD18f697C38f52437d791A8b90','0x4E944bb0399553c36461F678ff8c188D9A8f3FA3')).toBe(124);
+})
+
+test('testing x squared', () => {
+    expect(functions.func3(-2)).toBe(4);
+    expect(functions.func3(4)).toBe(16);
 })
