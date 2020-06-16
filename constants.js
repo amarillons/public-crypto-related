3dd7eab14a65f0ae5735a823897a514e96104853
a.f <akihiko.k.fujii@gmail.com>
2020-06-16 15:37:22 +0900

    Add constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+constants = {
+    projectId: process.env.INFURA_PROJECT_ID,
+    tokenAddress: process.env.TOKEN_ADDRESS,
+    eoaAddressForTesting: process.env.EOA_ADDRESS_TESTING,
+}
+
+module.exports = constants;
+
